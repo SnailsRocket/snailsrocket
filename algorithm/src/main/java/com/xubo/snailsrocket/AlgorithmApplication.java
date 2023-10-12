c332d1aa757836fa52718ad73385d2ab63c1c4ff
xubo <xubo@ecdigit.com>
2023-10-12 15:13:55 +0800

    项目初始化

---
+++ algorithm/src/main/java/com/xubo/snailsrocket/AlgorithmApplication.java
@@ -0,0 +1,17 @@
+package com.xubo.snailsrocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author xubo
+ * @Date 2023/10/12 15:10
+ * @Description：algorithm 算法 leetcode
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class AlgorithmApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmApplication.class);
+    }
+}
---
+++ designmodel/src/main/java/com/xubo/snailsrocket/DesignApplication.java
@@ -0,0 +1,17 @@
+package com.xubo.snailsrocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author xubo
+ * @Date 2023/10/12 14:59
+ * @Description：设计模式
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class DesignApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DesignApplication.class);
+    }
+}
